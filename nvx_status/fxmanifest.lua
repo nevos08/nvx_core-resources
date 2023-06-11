d2bb66d3010d17a801067824080ce669c6b6d2d3
Le0n <86106048+nevos08@users.noreply.github.com>
2023-06-11 18:01:53 +0200

    add meta stuff to fxmanifest.lua

--- nvx_status/fxmanifest.lua
+++ nvx_status/fxmanifest.lua
@@ -3,6 +3,11 @@ game "gta5"
 use_experimental_fxv2_oal "yes"
 lua54 "yes"
 
+author "Nevos"
+description "Simple status system for the NVX Framework"
+version "0.0.1"
+repository "https://github.com/nevos08/nvx_core-resources/tree/main/nvx_status"
+
 shared_scripts {
     "@nvx_core/imports.lua",
     "config.lua"

ca9fc9e9dd96d5de6dbd8576b2fbd1ec3f430f63
Le0n <86106048+nevos08@users.noreply.github.com>
2023-06-11 18:01:41 +0200

    initialize nvx_inventory

---
+++ nvx_inventory/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Nevos"
+description "Inventory system for the NVX Framework"
+version "0.0.1"
+repository "https://github.com/nevos08/nvx_core-resources/tree/main/nvx_inventory"

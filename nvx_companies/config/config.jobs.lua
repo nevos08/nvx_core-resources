f422f1dfc818f856f515f166c13ccc0bc33c4fa4
Nevos <86106048+nevos08@users.noreply.github.com>
2023-06-16 08:41:47 +0200

    initial things for nvx_companies

---
+++ nvx_companies/config/config.jobs.lua
@@ -0,0 +1,21 @@
+Config.Jobs = {
+    ["police"] = {
+        name = "police",
+        label = "Los Santos Police Dept.",
+        flags = {
+            hasDuty = true,
+            hasAccount = true,
+            hasVehicles = true,
+        },
+        grades = {
+            ["rookie"] = {
+                name = "rookie",
+                label = "Rookie",
+                salary = 70,
+                permissions = {
+
+                }
+            }
+        }
+    }
+}
---
+++ nvx_companies/config/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.Locales = "de"
+
+Config.Permissions = {    --! DO NOT CHANGE UNLESS YOU KNOW WHAT YOU'RE DOING
+    "money",              -- Allow a player to see the company account
+    "money_withdraw",     -- Allow a player to withdraw money
+    "money_deposit",      -- Allow a player to deposit money
+
+    "employees",          -- Allow a player to see all employees
+    "employees_grade",    -- Allow a player to change a players grade
+    "employees_salary",   -- Allow a player to change a players salary
+    "employees_vehicles", -- Allow a player to changer vehicle permissions for a player
+}
